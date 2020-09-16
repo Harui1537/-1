c3bcf6cb8be6ed7b489e8898ea8efa70ee23c09d
야기시타 토시유키 <dbgkgygod123@naver.com>
2020-09-16 15:14:06 +0900

    cheak

--- main/MyApp.java
+++ main/MyApp.java
@@ -10,13 +10,29 @@ import java.util.StringTokenizer;
  */
 public class MyApp
 {
+    public int[] makeToken(String str)
+    {
+        int[] array;
+        StringTokenizer st = new StringTokenizer(str,",");
+        while (st.hasMoreTokens()){
+            String a = st.nextToken();
+            int b = (int) a;
+            
+        }
+    }
+    
+    
     public static void main(String[] args)
     {
         int[] array;
         int i = 0;
         String data = "2,3,5,2,3,10";
-        StringTokenizer st = new StringTokenizer(data,",");
         
-       
+        
+        
+        
+        
+        
+        
     }
 }
